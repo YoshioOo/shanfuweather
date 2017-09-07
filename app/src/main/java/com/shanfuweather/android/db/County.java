b265da7bf4a36055e96d6bd9290b06db533e2e98
YoshioOo <951327634@qq.com>
2017-09-07 15:40:28 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/shanfuweather/android/db/County.java
@@ -0,0 +1,50 @@
+package com.shanfuweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 95132 on 2017/9/7.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountName() {
+        return countyName;
+    }
+
+    public void setCountName(String countName) {
+        this.countyName = countName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
