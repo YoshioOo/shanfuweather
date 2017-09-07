0ad0917a8ba35343e8f5ec13a5795cf0e4494041
YoshioOo <951327634@qq.com>
2017-09-07 22:40:26 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/shanfuweather/android/WeatherActivity.java
+++ app/src/main/java/com/shanfuweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.shanfuweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.shanfuweather.android.gson.Forecast;
 import com.shanfuweather.android.gson.Weather;
+import com.shanfuweather.android.service.AutoUpdateService;
 import com.shanfuweather.android.util.HttpUtil;
 import com.shanfuweather.android.util.Utility;
 
@@ -153,6 +155,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather",responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+                            Intent intent = new Intent(WeatherActivity.this, AutoUpdateService.class);
+                            startService(intent);
                         }else{
                             Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
                         }
